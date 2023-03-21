bc332b3385632c00ac4fd5f3d2341b231150d92f
hunghv2 <hunghv2@sapo.vn>
2023-03-21 18:37:35 +0700

    feat: customer

--- apps/beautiful-care/src/components/Input.tsx
+++ apps/beautiful-care/src/components/Input.tsx
@@ -1,42 +1,41 @@
 import classNames from 'classnames';
-import { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   title?: string;
   error?: string;
 }
 
-export default function Input({
-  value,
-  placeholder,
-  className,
-  onChange,
-  title,
-  error,
-  type,
-  ...props
-}: InputProps) {
-  return (
-    <div className="form-control w-full max-w-xs">
-      {title && (
-        <label className="label">
-          <span className="label-text">{title}</span>
-        </label>
-      )}
-      <input
-        type={type}
-        placeholder={placeholder}
-        className={classNames(
-          'input input-bordered w-full max-w-xs text-sm',
-          className
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { value, placeholder, className, onChange, title, error, type, ...props },
+    forwardedRef
+  ) => {
+    return (
+      <div className="form-control w-full max-w-xs">
+        {title && (
+          <label className="label">
+            <span className="label-text">{title}</span>
+          </label>
         )}
-        {...props}
-      />
-      {error && (
-        <label className="label">
-          <span className="label-text-alt">{error}</span>
-        </label>
-      )}
-    </div>
-  );
-}
+        <input
+          ref={forwardedRef}
+          type={type}
+          placeholder={placeholder}
+          className={classNames(
+            'input input-bordered w-full max-w-xs text-sm',
+            className
+          )}
+          {...props}
+        />
+        {error && (
+          <label className="label">
+            <span className="label-text-alt">{error}</span>
+          </label>
+        )}
+      </div>
+    );
+  }
+);
+
+export default Input;
--- apps/beautiful-care/src/pages/Customer.tsx
+++ apps/beautiful-care/src/pages/Customer.tsx
@@ -1,16 +1,28 @@
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { object, string } from 'zod';
 import { CustomerRequest } from '../apis/customer.api';
 import Input from '../components/Input';
 import Select from '../components/Select';
 import TextArea from '../components/TextArea';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function Customer() {
+  const variantSchema = object({
+    name: string()
+      .nonempty('Họ tên không được để trống')
+      .max(255, 'Họ tên tối đa 255 ký tự'),
+    phone: string()
+      .nonempty('Số điện thoại không được để trống')
+      .max(20, 'Số điện thoại tối đa 20 ký tự'),
+    email: string().max(50, 'Email tối đa 50 ký tự'),
+  });
+
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<CustomerRequest>();
+    formState: { isSubmitting, errors, isValid, isDirty },
+  } = useForm<CustomerRequest>({ resolver: zodResolver(variantSchema) });
 
   const onSubmit = (data: CustomerRequest) => {
     console.log(data);
@@ -22,18 +34,37 @@ export default function Customer() {
         Khách hàng
       </Link>
       <h2 className="text-xl font-semibold py-4">Thêm mới khách hàng</h2>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="grid lg:flex w-full gap-4"
-      >
-        <div className="bg-white border rounded shadow-sm p-4 lg:w-2/3 grid lg:grid-cols-2 gap-x-4">
-          <Input title="Họ tên" placeholder="Nhập họ tên" />
-          <Input title="Số điện thoại" placeholder="Nhập số điện thoại" />
-          <Input title="Email" placeholder="Nhập email" />
-          <Select title="Giới tính" />
+      <form onSubmit={handleSubmit(onSubmit, (errors) => console.log(errors))}>
+        <div className="grid lg:flex w-full gap-4">
+          <div className="bg-white border rounded shadow-sm p-4 lg:w-2/3 grid lg:grid-cols-2 gap-x-4">
+            <Input
+              title="Họ tên"
+              placeholder="Nhập họ tên"
+              {...register('name')}
+            />
+            <Input
+              title="Số điện thoại"
+              placeholder="Nhập số điện thoại"
+              {...register('phone')}
+            />
+            <Input
+              title="Email"
+              placeholder="Nhập email"
+              {...register('email')}
+            />
+            <Select title="Giới tính" />
+          </div>
+          <div className="bg-white border rounded shadow-sm p-4 lg:w-1/3">
+            <TextArea title="Ghi chú" />
+          </div>
         </div>
-        <div className="bg-white border rounded shadow-sm p-4 lg:w-1/3">
-          <TextArea title="Ghi chú" />
+        <div className="flex justify-end mt-4">
+          <Link to="/admin/customers">
+            <button className="btn">Hủy</button>
+          </Link>
+          <button className="btn btn-primary ml-2" type="submit">
+            Lưu
+          </button>
         </div>
       </form>
     </>
